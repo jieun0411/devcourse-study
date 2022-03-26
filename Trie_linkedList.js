3ec0c4a0b21b5f9963791c4ad70ee9c3a794f89b
jieun0411 <annakim0411@naver.com>
2022-03-26 20:46:04 +0900

    연결 리스트 이용한 트라이 구현

---
+++ Trie_linkedList.js
@@ -0,0 +1,51 @@
+//// 연결리스트 이용하여 트라이 구현 ////
+
+// 인접리스트 사용해서 구현
+class Node {
+  constructor(value = "") {
+    this.value = value;
+    this.childern = new Map();
+  }
+}
+
+class Trie {
+  //루트로 빈 노드 생성
+  constructor() {
+    this.root = new Node();
+  }
+
+  // 문자열 추가
+  insert(string) {
+    let curNode = this.root;
+
+    // 문자열을 하나씩 자르며 순회
+    for (const char of string) {
+      // 간선에 없다면 새롭게 추가
+      if (!curNode.childern.has(char)) {
+        curNode.childern.set(char, new Node(curNode.value + char));
+      }
+      // 다음 정점으로 이동
+      curNode = curNode.children.get(char);
+    }
+  }
+
+  // 문자열 존재하는지 체크
+  has(string) {
+    let curNode = this.root;
+
+    for (const char of string) {
+      if (!curNode.children.has(char)) {
+        return false;
+      }
+      curNode = curNode.childern.get(char);
+    }
+    return true;
+  }
+}
+
+const trie = new Trie();
+trie.insert("cat");
+trie.insert("can");
+console.log(trie.has("cat"));
+console.log(trie.has("can"));
+console.log(trie.has("cap"));
