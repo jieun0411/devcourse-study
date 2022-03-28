71ca10e780d5310d2bb0ef48a9ddce727d06b245
jieun0411 <annakim0411@naver.com>
2022-03-28 21:06:44 +0900

    DFS(깊이우선탐색)

---
+++ DFS.js
@@ -0,0 +1,21 @@
+//input 배열이나 노드 등의 값이 arr로 주어질때
+
+/*
+
+let visited = Array(arr.length).fill(false);
+
+const DFS = (L)=>{
+	if(L > 조건) return;
+    
+    for(let i = 0; i < arr.length; i++){
+        if(문제에 맞는 조건 && visited === false){
+          visited[i] = true;
+          DFS(L + 1);
+          visited[i] = false;
+    	}
+    }
+}
+
+DFS(1);
+
+*/
