816eb2733c2b16f2ad305bcf27df9375f7367d5c
jieun0411 <annakim0411@naver.com>
2022-03-29 15:02:07 +0900

    [실습문제] 큰 수 만들기

---
+++ p_makeABigNumber.js
@@ -0,0 +1,23 @@
+//// [살습문제] 큰 수 만들기 ////
+// 큰 값이 나오면 이전 값 중 더 작은 값은 전부 삭제한다.
+// 스택의 바닥에서부터 탑은 큰 수부터 작은 수로 나열되어야 하는 것!
+
+function solution(number, k) {
+  const stack = [];
+  let count = 0;
+
+  for (const item of number) {
+    while (count < k && stack[stack.length - 1] < item) {
+      stack.pop();
+      count += 1;
+    }
+    stack.push(item);
+  }
+
+  // [예외처리] 첫값이 가장 큰 경우 "987654"
+  while (count < k) {
+    stack.pop();
+    count += 1;
+  }
+  return stack.join("");
+}
